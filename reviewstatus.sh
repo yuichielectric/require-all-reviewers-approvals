37bb1719ef13cd704f2a783d51f92e985d7ff8c9
Yuichi Tanaka <yuichielectric@github.com>
2021-03-25 23:30:43 +0900

    Pass variables to script

--- reviewstatus.sh
+++ reviewstatus.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-NOT_APPROVED_REVIEWERS=$(gh api -X GET 'repos/${{ github.event.repository.owner }}/${{ github.event.repository.name}}/pulls/${{ github.event.number }}/reviews' --jq '[.[] | select(.state != "APPROVED")] | length')
-REQUESTED_REVIEWERS=$(gh api -X GET 'repos/${{ github.event.repository.owner}}/${{ github.event.repository.name}}/pulls/${{ github.event.number}}/requested_reviewers' --jq ".users | length")
+OWNER=$1
+REPO=$2
+NUMBER=$3
+SHA=$4
+
+NOT_APPROVED_REVIEWERS=$(gh api -X GET 'repos/${OWNER}/${REPO}/pulls/${NUMBER}/reviews' --jq '[.[] | select(.state != "APPROVED")] | length')
+REQUESTED_REVIEWERS=$(gh api -X GET 'repos/${OWNER}/${REPO}/pulls/${NUMBER}/requested_reviewers' --jq ".users | length")
 
 if [ ${NOT_APPROVED_REVIEWERS} -eq 0 ] && [ ${REQUESTED_REVIEWERS} -eq 0]; then
   STATE="success"
@@ -9,4 +14,4 @@ else
   STATE="failure"
 fi
 
-gh api -X POST '/repos/${{ github.event.repository.owner}}/${{ github.event.repository.name}}/statuses/${{ github.sha }}' -f state=${STATE}
+gh api -X POST '/repos/${OWNER}/${REPO}/statuses/${SHA}' -f state=${STATE}
