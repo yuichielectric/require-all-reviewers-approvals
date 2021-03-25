377a6837d708810205ffe0e86b0dc2a8e15660e3
Yuichi Tanaka <yuichielectric@github.com>
2021-03-25 23:51:06 +0900

    Fix single quote to double quote to expand variables

--- reviewstatus.sh
+++ reviewstatus.sh
@@ -6,10 +6,9 @@ OWNER=$1
 REPO=$2
 NUMBER=$3
 SHA=$4
-echo 'repos/${OWNER}/${REPO}/pulls/${NUMBER}/reviews'
-echo "repos/${OWNER}/${REPO}/pulls/${NUMBER}/reviews"
-NOT_APPROVED_REVIEWERS=$(gh api -X GET 'repos/${OWNER}/${REPO}/pulls/${NUMBER}/reviews' --jq '[.[] | select(.state != "APPROVED")] | length')
-REQUESTED_REVIEWERS=$(gh api -X GET 'repos/${OWNER}/${REPO}/pulls/${NUMBER}/requested_reviewers' --jq ".users | length")
+
+NOT_APPROVED_REVIEWERS=$(gh api -X GET "repos/${OWNER}/${REPO}/pulls/${NUMBER}/reviews" --jq '[.[] | select(.state != "APPROVED")] | length')
+REQUESTED_REVIEWERS=$(gh api -X GET "repos/${OWNER}/${REPO}/pulls/${NUMBER}/requested_reviewers" --jq ".users | length")
 
 if [ ${NOT_APPROVED_REVIEWERS} -eq 0 ] && [ ${REQUESTED_REVIEWERS} -eq 0]; then
   STATE="success"
@@ -17,4 +16,4 @@ else
   STATE="failure"
 fi
 
-gh api -X POST '/repos/${OWNER}/${REPO}/statuses/${SHA}' -f state=${STATE}
+gh api -X POST "/repos/${OWNER}/${REPO}/statuses/${SHA}" -f state=${STATE}
