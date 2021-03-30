2480bee671350b3f9cbc8677207313f38fd2dff0
Yuichi Tanaka <yuichielectric@github.com>
2021-03-30 19:03:23 +0900

    Refactor

--- reviewstatus.sh
+++ reviewstatus.sh
@@ -3,25 +3,29 @@
 set -x
 
 IFS=,
-STATE="success"
 read -ra REVIEWERS <<< "${REQUIRED_REVIEWERS}"
 NEED_REVIEW_BY=()
+
 for reviewer in "${REVIEWERS[@]}"; do
+  # PR owner cannot approve their own PR, so skip them.
   if [ "${reviewer}" = "${PR_OWNER}" ]; then
     continue
   fi
 
+  # Check if the required reviewer has approved the PR.
   REVIEWED=$(gh api -X GET "repos/${OWNER}/${REPO}/pulls/${PR_NUMBER}/reviews" --jq "[.[] | select(.state == \"APPROVED\" and .user.login == \"$reviewer\")] | length")
   if [ "$REVIEWED" -eq "0" ];
   then
-    STATE="failure"
+    # Review has not been approved by this reviewer.
     NEED_REVIEW_BY+=("${reviewer}")
   fi
 done
 
 if [ ${#NEED_REVIEW_BY[@]} -eq "0" ]; then
+  STATE="success"
   DESCRIPTION=''
 else
+  STATE="failure"
   DESCRIPTION="Reviews by ${NEED_REVIEW_BY[*]} are required."
 fi
 
